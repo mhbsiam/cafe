d0e1fa20bc9a89c00d8f98c41215517498f5d2af
Hasanul Banna Siam <65338389+mhbsiam@users.noreply.github.com>
2024-11-22 02:20:05 -0600

    Update CAFE.py

--- CAFE/bin/App/CAFE.py
+++ CAFE/bin/App/CAFE.py
@@ -47,8 +47,6 @@ st.markdown("---")
 # Documentation and citation section
 st.markdown("## Citation")
 st.markdown("""
-For detailed documentation on how to use CAFE, including tutorials and examples, please follow the tutorial.
-
 If you use CAFE in your research, please cite our paper:
 
 > Vardaman, Donald, Md Akkas Ali, Md Hasanul Banna Siam, Chase Bolding, Harrison Tidwell, Holly R. Stephens, Mallikarjun Patil, and Daniel J. Tyrrell. "Development of a Spectral Flow Cytometry Analysis Pipeline for High-Dimensional Immune Cell Characterization." The Journal of Immunology (2024) https://doi.org/10.4049/jimmunol.2400370.
